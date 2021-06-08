cb78e69fcfc15fb7e5731a02794b87b11122920c
Lukas Heinrich <lukas.heinrich@gmail.com>
2021-06-08 15:23:38 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,15 @@ namespace py = pybind11;
 
 class JetAuxContainer{
     public:
-        JetAuxContainer(double* pt, double* eta) : m_pt(pt), m_eta(eta){}
+        JetAuxContainer(double* pt, double* eta, size_t size) : m_pt(pt), m_eta(eta), m_size(size){}
         double pt(int index) const {return m_pt[index];}
         double eta(int index) const {return m_eta[index];}
+        size_t size() const {return m_size;}
     
     private:
         double* m_pt;
         double* m_eta;
+        size_t m_size;
 };
 
 class Jet {
@@ -28,7 +30,8 @@ class Jet {
 class JetContainer{
     public:
         JetContainer(const JetAuxContainer& aux) : m_aux(aux) {}
-        Jet at(int index){return Jet(index, m_aux);}
+        Jet at(int index) const {return Jet(index, m_aux);}
+        size_t size() const { return m_aux.size(); }
     private:
         const JetAuxContainer& m_aux;
 };
@@ -39,7 +42,12 @@ class JetTool {
             return j.pt()+j.eta();
         }
         std::vector<double> calibrate(const JetContainer& c){
-            std::vector<double> v;
+            std::vector<double> v(c.size());
+            for (size_t idx = 0; idx < v.size(); idx++){
+                std::cout << c.at(idx).pt() << " | " << c.at(idx).eta() << std::endl;
+                v[idx] = this->calibrate(c.at(idx));
+            }
+            return v;
         }
 };
 
@@ -61,14 +69,14 @@ py::array_t<double> calibrate(py::array_t<double> jet_pt, py::array_t<double> je
     double *jet_pt_array  = static_cast<double *>(buf1.ptr);
     double *jet_eta_array = static_cast<double *>(buf2.ptr);
 
-    JetAuxContainer jetaux(jet_pt_array,jet_eta_array);
+    JetAuxContainer jetaux(jet_pt_array,jet_eta_array, buf1.size);
     JetContainer jets(jetaux);
 
     double *jet_calibrated_array = static_cast<double *>(buf3.ptr);
 
-    for (size_t idx = 0; idx < buf1.shape[0]; idx++){
-        std::cout << jets.at(idx).pt() << " | " << jets.at(idx).eta() << std::endl;
-        jet_calibrated_array[idx] = jet_tool.calibrate(jets.at(idx));
+    auto tool_result = jet_tool.calibrate(jets);
+    for (size_t idx = 0; idx < buf1.shape[0]; idx++){ //todo make zero-copy
+        jet_calibrated_array[idx] = tool_result[idx];
     }
     return result;
 }
